9798b2e2306ab3e757d2ffa74da460a181e1b598
Luis <luiswar03@gmail.com>
2025-04-11 01:22:37 +0200

    Implemented more InputValidator

--- src/main/com/trakHive/Main.java
+++ src/main/com/trakHive/Main.java
@@ -1,5 +1,5 @@
 package main.com.trakHive;
-
+import cli.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello user, I am bored.");
--- src/main/com/trakHive/utils/InputValidator.java
+++ src/main/com/trakHive/utils/InputValidator.java
@@ -1,5 +1,8 @@
 package main.com.trakHive.utils;
+import java.time.DateTimeException;
+import java.time.LocalDate;
 import java.time.Year;
+import java.time.format.DateTimeParseException;
 
 public class InputValidator {
     public static boolean isValidYear(int year) {
@@ -9,10 +12,24 @@ public class InputValidator {
         return input != null && !input.trim().isEmpty();
     }
     public static boolean isValidEmail(String email) {
-
+        //TODO
     }
     public static boolean isValidRating(double rating, double min, double max) {
         return rating >= min && rating <= max;
+    }
+    public static boolean isValidDate(String dateStr) {
+        try {
+            LocalDate.parse(dateStr);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+    public static boolean isValidLength(String str, int minLengh, int maxLength) {
+        return str != null && str.length() >= minLengh && str.length() <= maxLength;
+    }
+    public static boolean isValidBoolean(String str) {
+        return "true".equalsIgnoreCase(str) || "false".equalsIgnoreCase(str);
     }
 
 }
