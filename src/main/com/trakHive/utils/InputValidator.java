10c5665b0b27a587c041c2390a8266cbdfb12c41
Luis <luiswar03@gmail.com>
2025-04-11 01:05:03 +0200

    added more InputValidator

--- src/main/com/trakHive/utils/InputValidator.java
+++ src/main/com/trakHive/utils/InputValidator.java
@@ -1,12 +1,18 @@
 package main.com.trakHive.utils;
+import java.time.Year;
 
 public class InputValidator {
     public static boolean isValidYear(int year) {
-        if (year >= 1800) {
-            return true;
-        }
+        return year > 0 && year <= Year.now().getValue();
     }
     public static boolean isNonEmpty(String input) {
+        return input != null && !input.trim().isEmpty();
+    }
+    public static boolean isValidEmail(String email) {
 
     }
+    public static boolean isValidRating(double rating, double min, double max) {
+        return rating >= min && rating <= max;
+    }
+
 }
